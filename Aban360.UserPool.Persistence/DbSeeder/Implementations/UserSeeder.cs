473070222b42db09c47ed2c569aa02b32169d556
sepehr shamsaie <mantera.sh@hotmail.com>
2025-02-04 14:56:29 +0330

    hangfire dashboard auth 01

--- Aban360.UserPool.Persistence/DbSeeder/Implementations/UserSeeder.cs
+++ Aban360.UserPool.Persistence/DbSeeder/Implementations/UserSeeder.cs
@@ -10,7 +10,7 @@ namespace Aban360.UserPool.Persistence.DbSeeder.Implementations
 {
     public class UserSeeder : IDataSeeder
     {
-        public int Order { get; set; } = 4;
+        public int Order { get; set; } = 6;
         public ClaimType ClaimType { get; private set; }
 
         private readonly IUnitOfWork _uow;
@@ -27,26 +27,37 @@ namespace Aban360.UserPool.Persistence.DbSeeder.Implementations
         {
             if (!_users.Any())
             {
+                var logInfo = LogInfoJson.Get();
+                var userId = Guid.NewGuid();
                 var programmer = new User()
                 {
                     DisplayName = "برنامه نویس",
                     FullName = "برنامه نویس",
                     Username = "programmer",
                     HasTwoStepVerification = false,
-                    Id = Guid.NewGuid(),
-                    InsertLogInfo = LogInfoJson.Get(),
+                    Id = userId,
+                    InsertLogInfo = logInfo,
                     InvalidLoginAttemptCount = 0,
                     Mobile = "09130000000",
                     MobileConfirmed = false,
                     ValidFrom = DateTime.Now,
                     Password = await SecurityOperations.GetSha512Hash("123456"),
                     Hash = string.Empty,
-                    SerialNumber=Guid.NewGuid().ToString("n"),
+                    SerialNumber = Guid.NewGuid().ToString("n"),
+                    UserRoles = GetProgrammerRoles(userId, logInfo)
                     //UserClaims= new List<UserClaim>
                 };
                 _users.Add(programmer);
                 _uow.SaveChanges();
             }           
+        }
+        private ICollection<UserRole> GetProgrammerRoles(Guid userId, string logInfo)
+        {
+            var userRoles = new List<UserRole>()
+            {
+                new UserRole() {Hash=string.Empty,UserId=userId,InsertGroupId=Guid.NewGuid(),RoleId=3,ValidFrom=DateTime.Now,InsertLogInfo=logInfo},
+            };
+            return userRoles;
         }
     }
 }
