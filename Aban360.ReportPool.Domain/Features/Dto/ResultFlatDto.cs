4947c6fdbc372d41bd44c172de5d19815e84245d
ZEtehadi <zetehadi82@gmail.com>
2025-02-22 17:29:53 +0330

    SummeryQueryService

---
+++ Aban360.ReportPool.Domain/Features/Dto/ResultFlatDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Aban360.ReportPool.Persistence.Queries.Implementations
+{
+    public record ResultFlatDto
+    {
+        public string? PostalCode { get; set; }
+        public short Storey { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Aban360.ReportPool.Persistence/Queries/Contracts/ISiphonSummeryQueryService.cs
@@ -0,0 +1,7 @@
+﻿namespace Aban360.ReportPool.Persistence.Queries.Implementations
+{
+    public interface ISiphonSummeryQueryService
+    {
+        Task<SiphonSummaryDto> GetSummery(string billId);
+    }
+}
