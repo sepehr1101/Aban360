99c0e5f980ab6635627ba7580c4d2bfaa38f4e65
ZEtehadi <zetehadi82@gmail.com>
2025-04-06 09:23:30 +0330

    BlobPool

---
+++ Aban360.BloblPool.Persistence/Extensions/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿namespace Aban360.BloblPool.Persistence.Extensions
+{
+    public static class ConfigureServices
+    {
+        //public static void AddCalculationPoolPersistenceInjections(this IServiceCollection services)
+        //{
+        //    services.Scan(scan =>
+        //        scan
+        //            .FromAssemblies(Assembly.GetExecutingAssembly())
+        //            .AddClasses(publicOnly: false)
+        //            .UsingRegistrationStrategy(RegistrationStrategy.Append)
+        //            .AsImplementedInterfaces()
+        //            .WithScopedLifetime());
+        //}
+    }
+}
