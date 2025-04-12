a6f775c174cc33c2171389cfcc3a01ffb291b342
sepehr shamsaie <mantera.sh@hotmail.com>
2025-04-12 11:17:06 +0330

    bug fix

--- Aban360.ReportPool.Persistence/Contexts/Implementations/ReportPoolDbSets.cs
+++ Aban360.ReportPool.Persistence/Contexts/Implementations/ReportPoolDbSets.cs
@@ -1,7 +1,10 @@
-﻿namespace Aban360.ReportPool.Persistence.Contexts.Implementations
+﻿using Aban360.UserPool.Persistence.Scaffold;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aban360.ReportPool.Persistence.Contexts.Implementations
 {
     public partial class ReportPoolContext
     {
-
+        public virtual DbSet<DynamicReport> DynamicReports { get; set; }
     }
 }
