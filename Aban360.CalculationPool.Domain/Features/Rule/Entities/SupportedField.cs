9f0c6b85310e5b82c484b7d35faa482d6a06099a
sepehr shamsaie <mantera.sh@hotmail.com>
2025-04-14 13:58:49 +0330

    supported operation and field

---
+++ Aban360.CalculationPool.Domain/Features/Rule/Entities/SupportedField.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aban360.CalculationPool.Domain.Features.Rule.Entities
+{
+    [Table(nameof(SupportedField))]
+    public class SupportedField
+    {
+        public short Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Desciption { get; set; } = default!;
+    }
+}
