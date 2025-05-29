0c85433cba4df0f915c344f459f1aa7b27df5869
sepehr shamsaie <mantera.sh@hotmail.com>
2025-05-29 06:12:35 +0330

    remove unneseccery impact sign

--- Aban360.CalculationPool.Domain/Features/Bill/Dtos/Commands/ImpactSignDeleteDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Aban360.CalculationPool.Domain.Constants;
-
-namespace Aban360.CalculationPool.Domain.Features.Bill.Dtos.Commands
-{
-    public record ImpactSignDeleteDto
-    {
-        public ImpactSignEnum Id { get; set; }
-    }
-}
