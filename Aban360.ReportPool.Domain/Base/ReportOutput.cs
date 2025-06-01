aeab66dae7d93dfac1225aa729a58ad4d67465ea
sepehr shamsaie <mantera.sh@hotmail.com>
2025-06-01 11:52:52 +0330

    report output

---
+++ Aban360.ReportPool.Domain/Base/ReportOutput.cs
@@ -0,0 +1,15 @@
+﻿namespace Aban360.ReportPool.Domain.Base
+{
+    public record ReportOutput<TReportHeader,TReportData>
+    {
+        public string Title { get; } = default!;
+        public TReportHeader ReportHeader { get; }= default!;
+        public TReportData ReportData { get; } = default!;
+        public ReportOutput(string title, TReportHeader header, TReportData data)
+        {
+            Title = title;
+            ReportData = data;
+            ReportHeader = header;
+        }
+    }
+}
