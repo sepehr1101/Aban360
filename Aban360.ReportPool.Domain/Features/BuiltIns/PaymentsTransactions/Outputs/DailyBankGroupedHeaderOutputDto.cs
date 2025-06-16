747a6fc711964aa2c0d1ee675efbac971217cd80
ZEtehadi <zetehadi82@gmail.com>
2025-06-16 10:29:53 +0330

    DailyBankGroup

--- Aban360.ReportPool.Domain/Features/BuiltIns/PaymentsTransactions/Outputs/DailyBankGroupedHeaderOutputDto.cs
+++ Aban360.ReportPool.Domain/Features/BuiltIns/PaymentsTransactions/Outputs/DailyBankGroupedHeaderOutputDto.cs
@@ -2,6 +2,10 @@
 {
     public record DailyBankGroupedHeaderOutputDto
     {
-
+        public string ReportDate { get; set; }
+        public string FromDateJalali { get; set; }
+        public string ToDateJalali { get; set; }
+        public long? FromAmount { get; set; }
+        public long? ToAmount { get; set; }
     }
 }
