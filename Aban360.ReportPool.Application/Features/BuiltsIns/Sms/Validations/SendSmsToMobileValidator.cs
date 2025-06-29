ecaaa56f0c2fae3556b4dfedde29dfeed4de239f
ZEtehadi <zetehadi82@gmail.com>
2025-06-29 13:01:49 +0330

    SendSmsToMobile Report

---
+++ Aban360.ReportPool.Application/Features/BuiltsIns/Sms/Validations/SendSmsToMobileValidator.cs
@@ -0,0 +1,25 @@
+﻿using Aban360.Common.Literals;
+using Aban360.ReportPool.Application.Features.Base.Validations;
+using Aban360.ReportPool.Domain.Features.BuiltIns.Sms.Inputs;
+using FluentValidation;
+
+namespace Aban360.ReportPool.Application.Features.BuiltsIns.Sms.Validations
+{
+     public class SendSmsToMobileValidator : BaseValidator<SendSmsToMobileInputDto>
+    {
+        public SendSmsToMobileValidator()
+        {
+            RuleFor(installation => installation.FromDateJalali)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(installation => installation.ToDateJalali)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(installation => installation.Mobile)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+        }
+    }
+}
---
+++ Aban360.ReportPool.Domain/Features/BuiltIns/Sms/Outputs/SendSmsToMobileDataOutputDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Aban360.ReportPool.Domain.Features.BuiltIns.Sms.Outputs
+{
+    public record SendSmsToMobileDataOutputDto
+    {
+        public string Text { get; set; }
+        public string SendTime { get; set; }
+        public string SendDate { get; set; }
+        public string FinalDeliveryStateTitle { get; set; }
+    }
+}
