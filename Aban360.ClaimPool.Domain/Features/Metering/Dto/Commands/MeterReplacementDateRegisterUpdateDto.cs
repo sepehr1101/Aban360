f1c647e6f841c83db7516300cd5ab0f286c4c679
ZEtehadi <zetehadi82@gmail.com>
2025-07-08 14:19:28 +0330

    EmptyUnit/Household Register

---
+++ Aban360.ClaimPool.Domain/Features/Metering/Dto/Commands/MeterReplacementDateRegisterUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Aban360.ClaimPool.Domain.Features.Metering.Dto.Commands
+{
+    public record MeterReplacementDateRegisterUpdateDto
+    {
+        public string  BillId { get; set; }
+        public string ReplacementDateJalali { get; set; }
+        public string MeterNumber { get; set; }
+    }
+}
---
+++ Aban360.ClaimPool.Persistence/Features/Metering/Commands/Implementations/MeterReplacementDateRegisterQueryService.cs
@@ -0,0 +1,29 @@
+﻿using Aban360.ClaimPool.Domain.Features.Metering.Dto.Commands;
+using Aban360.ClaimPool.Persistence.Features.Metering.Commands.Contracts;
+using Aban360.Common.Db.Dapper;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Aban360.ClaimPool.Persistence.Features.Metering.Commands.Implementations
+{
+    internal sealed class MeterReplacementDateRegisterQueryService : AbstractBaseConnection, IMeterReplacementDateRegisterQueryService
+    {
+        public MeterReplacementDateRegisterQueryService(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+        public async Task Update(MeterReplacementDateRegisterUpdateDto input)
+        {
+            string meterReplacementDateRegisterQuery = GetMeterReplacementDateRegisterQuery();
+            var @params = new { };
+            var result = await _sqlReportConnection.ExecuteAsync(meterReplacementDateRegisterQuery, @params);
+        }
+
+
+        private string GetMeterReplacementDateRegisterQuery()
+        {
+            return @"";
+        }
+
+    }
+}
