3e670296eadce4caff0ee2d0888be46803f4a11c
ZEtehadi <zetehadi82@gmail.com>
2025-07-09 16:01:51 +0330

    AgentWarnings Report

---
+++ Aban360.ReportPool.Persistence/Features/BuiltIns/PaymentTransactions/Contracts/IAgentWarningsQueryService.cs
@@ -0,0 +1,11 @@
+﻿using Aban360.ReportPool.Domain.Base;
+using Aban360.ReportPool.Domain.Features.BuiltIns.ServiceLinkTransaction.Inputs;
+using Aban360.ReportPool.Domain.Features.BuiltIns.ServiceLinkTransaction.Outputs;
+
+namespace Aban360.ReportPool.Persistence.Features.BuiltIns.PaymentTransactions.Contracts
+{
+    public interface IAgentWarningsQueryService
+    {
+        Task<ReportOutput<AgentWarningsHeaderOutputDto, AgentWarningsDataOutputDto>> GetInfo(AgentWarningsInputDto input);
+    }
+}
