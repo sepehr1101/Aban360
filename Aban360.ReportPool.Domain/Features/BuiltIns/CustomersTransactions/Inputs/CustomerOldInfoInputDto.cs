6f518a7a9eea7a59a37756d66326166746803bb7
ZEtehadi <zetehadi82@gmail.com>
2025-07-14 11:25:14 +0330

    GetCustomerOldInfo Bridge

---
+++ Aban360.ReportPool.Domain/Features/BuiltIns/CustomersTransactions/Inputs/CustomerOldInfoInputDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Aban360.ReportPool.Domain.Features.BuiltIns.CustomersTransactions.Inputs
+{
+    public record CustomerOldInfoInputDto
+    {
+        public int ZoneId { get; set; }
+        public int CustomerNumber { get; set; }
+    }
+}
