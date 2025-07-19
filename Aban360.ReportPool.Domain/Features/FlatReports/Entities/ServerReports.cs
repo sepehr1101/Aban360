bd373ad9ad8d3d5d8f9bdf0f262e6f388eef807a
ZEtehadi <zetehadi82@gmail.com>
2025-07-19 14:13:12 +0330

    ServerReports

---
+++ Aban360.ReportPool.Domain/Features/FlatReports/Entities/ServerReports.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aban360.ReportPool.Domain.Features.FlatReports.Entities
+{
+    [Table(nameof(ServerReports))]
+    public class ServerReports
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string ReportName { get; set; }
+        public string ReportPath { get; set; }
+        public Guid CompletionId { get; set; }
+        public string CompletionDateJalali { get; set; }
+        public string InsertDateJalali { get; set; }
+        public string ErrorDateJalali { get; set; }
+        public bool IsInformed { get; set; }
+    }
+}
---
+++ Aban360.ReportPool.Persistence/Features/FlatReports/Queries/Contracts/IServerReportsGetAllService.cs
@@ -0,0 +1,9 @@
+﻿using Aban360.ReportPool.Domain.Features.FlatReports.Dto.Queries;
+
+namespace Aban360.ReportPool.Persistence.Features.FlatReports.Queries.Contracts
+{
+    public interface IServerReportsGetAllService
+    {
+        Task<IEnumerable<ServerReportsGetDto>> Get(Guid userId);
+    }
+}
