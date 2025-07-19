9c63d2b1b8051415cd106017183a87a642658d73
sepehr shamsaie <mantera.sh@hotmail.com>
2025-07-19 17:24:16 +0330

    report generator

---
+++ Aban360.Api/Cronjobs/ReportGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Aban360.ReportPool.Domain.Base;
+
+namespace Aban360.Api.Cronjobs
+{
+    internal static class ReportGenerator
+    {
+        internal static async Task FireAndInform<TReportInput,THead,TData>(TReportInput reportInput,CancellationToken cancellationToken,Func<TReportInput, CancellationToken,Task<ReportOutput<THead,TData>>> GetData)
+        {
+            //Sample:  await GenerateReports.FireAndInform(inputDto, cancellationToken, _emptyUnit.Handle);
+
+            //Insert ServerReport
+            ReportOutput<THead, TData> reportOutput= await GetData(reportInput,cancellationToken);
+            //Complete ServerReport
+
+            //send events via signalR
+        }
+    }
+}
