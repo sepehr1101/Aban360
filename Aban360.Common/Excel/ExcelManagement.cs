e77338435b50a4da97e35690afba8f6cab7642b5
ZEtehadi <zetehadi82@gmail.com>
2025-07-19 12:18:13 +0330

    ExcelManagement

---
+++ Aban360.Common/Excel/ExcelManagement.cs
@@ -0,0 +1,134 @@
+﻿using Aban360.Common.Exceptions;
+using Aban360.Common.Literals;
+using DNTPersianUtils.Core;
+using MiniExcelLibs;
+using System.Reflection;
+
+namespace Aban360.Common.Excel
+{
+    public static class ExcelManagement
+    {
+        public static string pathBase { get; set; } = $"AppData\\Excels\\";
+        public static int maxDataCount { get; set; } = 1000000;
+        public static async Task<string> ExportToExcelAsync<THeader, TData>(THeader tHeader, IEnumerable<TData> tData, string reportName)
+        {
+            Validation(tHeader, tData);
+
+            var excelfile = new Dictionary<string, object>();
+            var sheetCount = tData.Count() / maxDataCount;
+
+            excelfile[ExceptionLiterals.Header] = new List<Dictionary<string, object>> { TranslateHeader(tHeader) };
+            for (int i = 0; i < sheetCount + 1; i++)
+            {
+                var sheetData = tData.Skip(i * maxDataCount).Take(maxDataCount).ToList();
+                excelfile[ExceptionLiterals.Page(i+1)] = TranslateData(sheetData);
+            }
+
+            string path = GetPath(reportName);
+            await MiniExcel.SaveAsAsync(path, excelfile);
+            return path;
+        }
+        private static string GetPath(string reportName)
+        {
+            string reportTitle = reportName
+                          .Replace('/', '-')
+                          .Replace(':', '-')
+                          .Replace(' ', '_');
+
+            var persianDateTime = DateTime
+                                        .Now
+                                        .ToShortPersianDateTimeString()
+                                        .Replace('/', '-')
+                                        .Replace(':', '-')
+                                        .Replace(' ', '_');
+
+            return $"{pathBase}{reportTitle}_{persianDateTime}.xlsx";
+        }
+        private static void Validation<THeader, TData>(THeader tHeader, IEnumerable<TData> tData)
+        {
+            if (tHeader == null || tData == null)
+                throw new BaseException(ExceptionLiterals.CantGenarateExcelWithNullData);
+        }
+
+        private static Dictionary<string, object> TranslateHeader<T>(T data)
+        {
+            var props = GetOrderedProperties(data);
+            var persianProp = GetPersianProperty();
+            var row = new Dictionary<string, object>();
+            foreach (var prop in props)
+            {
+                var propName = persianProp?.ContainsKey(prop.Name) == true ? persianProp[prop.Name] : prop.Name;
+                row[propName] = prop.GetValue(data);
+            }
+            return row;
+        }
+
+        private static List<Dictionary<string, object>> TranslateData<T>(IEnumerable<T> data)
+        {
+            var result = new List<Dictionary<string, object>>();
+
+            var enumerator = data.GetEnumerator();
+            if (!enumerator.MoveNext()) return result;
+
+            var first = enumerator.Current;
+            var props = GetOrderedProperties(first);
+            var persianProp = GetPersianProperty();
+            do
+            {
+                var row = new Dictionary<string, object>();
+                foreach (var prop in props)
+                {
+                    var propName = persianProp?.ContainsKey(prop.Name) == true ? persianProp[prop.Name] : prop.Name;
+                    row[propName] = prop.GetValue(enumerator.Current) ?? "";
+                }
+                result.Add(row);
+            }
+            while (enumerator.MoveNext());
+
+            return result;
+        }
+        private static List<PropertyInfo> GetOrderedProperties(object obj)
+        {
+            return obj.GetType()
+                      .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                      .OrderBy(p => p.MetadataToken)
+                      .ToList();
+        }
+
+        private static Dictionary<string, string> GetPersianProperty()
+        {
+            return new Dictionary<string, string>()
+            {
+                {"CustomerNumber", "شماره ردیف"},
+                {"ReadingNumber", "شماره اشتراک"},
+                {"FirstName", "نام"},
+                {"Surname", "نام خانوادگی"},
+                {"UsageTitle", "نوع کاربری"},
+                {"MeterDiameterTitle", "قطر کنتور"},
+                {"EventDateJalali", "تاریخ رویداد"},
+                {"Address", "آدرس"},
+                {"DomesticUnit", "واحد مسکونی"},
+                {"CommercialUnit", "واحد تجاری"},
+                {"OtherUnit", "سایر واحدها"},
+                {"BillId", "شناسه قبض"},
+                {"UseStateTitle", "نوع واگذاری"},
+                {"EmptyUnit", "واحد خالی"},
+                {"ZoneId", "کد منطقه"},
+                {"ZoneTitle", "نام منطقه"},
+                {"RegionId", "کد ناحیه"},
+                {"RegionTitle", "عنوان ناحیه"},
+                {"NationalCode", "کد ملی"},
+                {"PostalCode", "کد پستی"},
+                {"PhoneNumber", "شماره تلفن"},
+                {"FatherName", "نام پدر"},
+                {"FromReadingNumber", "از شماره اشتراک"},
+                {"ToReadingNumber", "تا شماره اشتراک"},
+                {"FromDateJalali", "از تاریخ"},
+                {"ToDateJalali", "تا تاریخ"},
+                {"ReportDateJalali", "تاریخ گزارش"},
+                {"ReportCount", "تعداد سطر"},
+            };
+
+        }
+    }
+}
--- Aban360.ReportPool.Domain/Features/BuiltIns/ServiceLinkTransaction/Inputs/SewageWaterInstallationInputDto.cs
+++ Aban360.ReportPool.Domain/Features/BuiltIns/ServiceLinkTransaction/Inputs/SewageWaterInstallationInputDto.cs
@@ -7,8 +7,8 @@
         public string FromDateJalali { get; set; }
         public string ToDateJalali { get; set; }
 
-        public  string FromReadingNumber { get; set; }
-        public string ToReadingNumber { get; set; }
+        public string? FromReadingNumber { get; set; }
+        public string? ToReadingNumber { get; set; }
         public ICollection<int> ZoneIds { get; set; }
     }
 }
