4eea4b5e3ef1ad75e4358cc9a5c54335f4676d1e
ZEtehadi <zetehadi82@gmail.com>
2025-07-20 12:16:32 +0330

    ReportServerPath

---
+++ Aban360.ReportPool.Application/Features/FlatReports/Handler/Queries/Contracts/IServerReportsGetFilePathHandler.cs
@@ -0,0 +1,9 @@
+﻿using Aban360.ReportPool.Domain.Features.FlatReports.Dto.Queries;
+
+namespace Aban360.ReportPool.Application.Features.FlatReports.Handler.Queries.Contracts
+{
+    public interface IServerReportsGetFilePathHandler
+    {
+        Task<ServerReportsGetFilePathDto> Handle(Guid userId, CancellationToken cancellationToken);
+    }
+}
---
+++ Aban360.ReportPool.Application/Features/FlatReports/Handler/Queries/Implementations/ServerReportsGetFilePathHandler.cs
@@ -0,0 +1,22 @@
+﻿using Aban360.Common.Extensions;
+using Aban360.ReportPool.Domain.Features.FlatReports.Dto.Queries;
+using Aban360.ReportPool.Persistence.Features.FlatReports.Queries.Contracts;
+
+namespace Aban360.ReportPool.Application.Features.FlatReports.Handler.Queries
+{
+    internal sealed class ServerReportsGetFilePathHandler : IServerReportsGetFilePathHandler
+    {
+        private readonly IServerReportsGetFilePathService _serverReportsGetFilePathService;
+        public ServerReportsGetFilePathHandler(IServerReportsGetFilePathService serverReportsGetFilePathService)
+        {
+            _serverReportsGetFilePathService = serverReportsGetFilePathService;
+            _serverReportsGetFilePathService.NotNull(nameof(serverReportsGetFilePathService));
+        }
+
+        public async Task<ServerReportsGetFilePathDto> Handle(Guid userId, CancellationToken cancellationToken)
+        {
+            var result = await _serverReportsGetFilePathService.Get(userId);
+            return result;
+        }
+    }
+}
