2b7938ae729c25e5bf6f30c3b35ec5420921297e
ZEtehadi <zetehadi82@gmail.com>
2025-07-20 15:53:27 +0330

    ZoneAll

---
+++ Aban360.ReportPool.Application/Features/BuiltsIns/CustomersTransactions/Handlers/Implementations/ZoneAllHandler.cs
@@ -0,0 +1,23 @@
+﻿using Aban360.Common.Extensions;
+using Aban360.ReportPool.Application.Features.BuiltsIns.CustomersTransactions.Handlers.Contracts;
+using Aban360.ReportPool.Domain.Features.BuiltIns.CustomersTransactions.Outputs;
+using Aban360.ReportPool.Persistence.Features.BuiltIns.CustomersTransactions.Contracts;
+
+namespace Aban360.ReportPool.Application.Features.BuiltsIns.CustomersTransactions.Handlers.Implementations
+{
+    internal sealed class ZoneAllHandler : IZoneAllHandler
+    {
+        private readonly IZoneQueryService _zoneQueryService;
+        public ZoneAllHandler(IZoneQueryService zoneQueryService)
+        {
+            _zoneQueryService = zoneQueryService;
+            _zoneQueryService.NotNull(nameof(zoneQueryService));
+        }
+
+        public async Task<IEnumerable<UserZoneIdsOutputDto>> Handle(CancellationToken cancellationToken)
+        {
+            IEnumerable<UserZoneIdsOutputDto> zone = await _zoneQueryService.Get();
+            return zone;
+        }
+    }
+}
