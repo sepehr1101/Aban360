a9a57de944e214f1056802aeb156fb3962f6ad85
sepehr shamsaie <mantera.sh@hotmail.com>
2025-07-27 13:09:33 +0330

    base old tariff

--- Aban360.OldCalcPool.Domain/Features/Processing/Dto/Queries/Input/MeterInfoInputDto.cs
+++ Aban360.OldCalcPool.Domain/Features/Processing/Dto/Queries/Input/MeterInfoInputDto.cs
@@ -2,8 +2,8 @@
 {
     public record MeterInfoInputDto
     {
-        public string BillId { get; set; }
-        public string CurrentDateJalali { get; set; }
+        public string BillId { get; set; } = default!;
+        public string CurrentDateJalali { get; set; } = default!;
         public int CurrentMeterNumber { get; set; }
     }
 }
