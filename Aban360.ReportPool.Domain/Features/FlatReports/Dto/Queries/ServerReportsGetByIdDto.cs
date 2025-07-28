9a4b622673e542001c419e53f41e804052ee886a
sepehr shamsaie <mantera.sh@hotmail.com>
2025-07-28 08:24:27 +0330

    hub

--- Aban360.ReportPool.Domain/Features/FlatReports/Dto/Queries/ServerReportsGetByIdDto.cs
+++ Aban360.ReportPool.Domain/Features/FlatReports/Dto/Queries/ServerReportsGetByIdDto.cs
@@ -4,12 +4,12 @@
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public string ReportName { get; set; }
-        public string ReportPath { get; set; }
-        public string ConnectionId { get; set; }
-        public string CompletionDateTimeJalali { get; set; }
-        public string InsertDateTimeJalali { get; set; }
-        public string ErrorDateTimeJalali { get; set; }
+        public string? ReportName { get; set; }
+        public string? ReportPath { get; set; }
+        public string? ConnectionId { get; set; }
+        public string? CompletionDateTimeJalali { get; set; }
+        public string? InsertDateTimeJalali { get; set; }
+        public string? ErrorDateTimeJalali { get; set; }
         public bool IsInformed { get; set; }
         public string HeaderType { get; set; } = default!;
         public string DataType { get; set; } = default!;
