70b35ee8525657953d05072d25bcf9f98567c030
ZEtehadi <zetehadi82@gmail.com>
2025-07-30 09:42:34 +0330

    Multiplier

---
+++ Aban360.OldCalcPool.Domain/Features/Rules/Dto/Queries/ZaribInputDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Aban360.OldCalcPool.Domain.Features.Rules.Dto.Queries
+{
+    public record ZaribInputDto
+    {
+        public int ZoneId { get; set; }
+        public string FromDateJalali { get; set; }
+        public string ToDateJalali { get;set; }
+        public ZaribInputDto(int _zoneId,string _fromDate,string _toDate)
+        {
+            ZoneId= _zoneId;
+            FromDateJalali= _fromDate;
+            ToDateJalali= _toDate;
+        }
+    }
+}
