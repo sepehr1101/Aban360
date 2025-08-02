575ce1eea3a4978f035567404fb56bb6f5fec0fb
ZEtehadi <zetehadi82@gmail.com>
2025-08-02 09:07:26 +0330

    Process OlbAbBaha

--- Aban360.OldCalcPool.Application/Features/Processing/Handlers/Commands/Contracts/IProcessing.cs
+++ Aban360.OldCalcPool.Application/Features/Processing/Handlers/Commands/Contracts/IProcessing.cs
@@ -1,11 +1,13 @@
-﻿using Aban360.OldCalcPool.Domain.Features.Processing.Dto.Commands;
+﻿using Aban360.OldCalcPool.Application.Features.Processing.Handlers.Commands.Implementations;
+using Aban360.OldCalcPool.Domain.Features.Processing.Dto.Commands;
 using Aban360.OldCalcPool.Domain.Features.Processing.Dto.Queries.Input;
+using static Aban360.OldCalcPool.Application.Features.Processing.Handlers.Commands.Implementations.Processing;
 
 namespace Aban360.OldCalcPool.Application.Features.Processing.Handlers.Commands.Contracts
 {
     public interface IProcessing
     {
         Task Handle(ConsumptionInputDto intput, CancellationToken cancellationToken);
-        Task Handle(MeterInfoInputDto input, CancellationToken cancellationToken);
+        Task<ProcessDetailOutputDto> Handle(MeterInfoInputDto input, CancellationToken cancellationToken);
     }
 }
