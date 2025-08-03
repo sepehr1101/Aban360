e12e80adcc6aa95fb39d3026fe2d6a02392678f8
sepehr shamsaie <mantera.sh@hotmail.com>
2025-08-03 13:42:19 +0330

    enable stream to download

--- Aban360.Api/Controllers/V1/ReportPool/FlatReports/Queries/ServerReportsDownloadController.cs
+++ Aban360.Api/Controllers/V1/ReportPool/FlatReports/Queries/ServerReportsDownloadController.cs
@@ -4,7 +4,6 @@ using Aban360.Common.Extensions;
 using Aban360.Common.Literals;
 using Aban360.ReportPool.Application.Features.FlatReports.Handler.Queries.Contracts;
 using Aban360.ReportPool.Domain.Features.FlatReports.Dto.Queries;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aban360.Api.Controllers.V1.ReportPool.FlatReports.Queries
@@ -22,18 +21,22 @@ namespace Aban360.Api.Controllers.V1.ReportPool.FlatReports.Queries
         [HttpGet]
         [Route("download/{id}")]
         [ProducesResponseType(typeof(ApiResponseEnvelope<ServerReportsGetFilePathDto>), StatusCodes.Status200OK)]
-        [AllowAnonymous]
         public async Task<FileResult> Download(Guid id,CancellationToken cancellationToken)
         {
             ServerReportsGetFilePathDto result = await _serverReportsGetFilePathandler.Handle(id, cancellationToken);
+            string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
             if (!System.IO.File.Exists(result.ReportPath))
+            {
                 throw new BaseException(ExceptionLiterals.NotFoundFile);
+            }
 
             string filePath = Path.GetFullPath(result.ReportPath);
             string fileName = Path.GetFileName(filePath);
 
-            return PhysicalFile(filePath, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return File(stream, excelContentType, fileName, enableRangeProcessing: true); // support resume and stream
+           
         }
     }
 }
