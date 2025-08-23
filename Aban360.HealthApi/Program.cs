9c93272a2a26218321b8929cfd3b82e4c29302a7
sepehr shamsaie <mantera.sh@hotmail.com>
2025-08-23 10:44:19 +0330

    basic health checking

--- Aban360.HealthApi/Program.cs
+++ Aban360.HealthApi/Program.cs
@@ -1,4 +1,8 @@
+using Aban360.HealthApi.Extensions.Aban360.HealthApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCustomHealthChecks(builder.Configuration);
 
 // Add services to the container.
 
@@ -8,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapCustomHealthChecks();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
