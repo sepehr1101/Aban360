7920e3502ba491bf6e8493b670148618c742c1be
sepehr shamsaie <mantera.sh@hotmail.com>
2025-09-22 16:20:06 +0330

    bridge changes

---
+++ Aban360.BrdigeApi/Controllers/V1/ServiceLink/ServiceLinkCalculationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aban360.BrdigeApi.Controllers.V1.ServiceLink
+{
+    [Route("v1/service-link")]
+    public class ServiceLinkCalculationController : BaseController
+    {
+        [HttpPost]
+        [Route("connection-fee")]
+        [AllowAnonymous]
+        public IActionResult CalculateConnectionFee()
+        {
+            Fee fee = new()
+            {
+                FeeItems = new[] { new FeeItem(27_000_000, 1, "حق انشعاب") },
+                AmountSum = 27_000_000
+            };
+            return Ok(fee);
+        }
+    }
+
+    public record Fee
+    {
+        public ICollection<FeeItem> FeeItems { get; set; } = default!;
+        public long AmountSum { get; set; }
+    }
+    public record FeeItem
+    {
+        public long Amount { get; set; }
+        public int ItemId { get; set; }
+        public string ItemTitle { get; set; } = default!;
+        public FeeItem(long amount, int itemId, string itemTitle)
+        {
+            Amount=amount;
+            ItemId=itemId;
+            ItemTitle=itemTitle;
+        }
+    }
+}
--- Aban360.BrdigeApi/Extensions/ConfigureDependencies.cs
+++ Aban360.BrdigeApi/Extensions/ConfigureDependencies.cs
@@ -1,30 +1,15 @@
 ﻿using Aban360.UserPool.Application.Extensions;
 using Aban360.UserPool.Persistence.Extensions;
-using Aban360.LocationPool.Application.Extensions;
-using Aban360.LocationPool.Persistence.Extensions;
-using Aban360.LocationPool.GatewayAdhoc.Extentions;
-using Aban360.ClaimPool.Application.Extentions;
-using Aban360.ClaimPool.Persistence.Extensions;
 using Aban360.ReportPool.Persistence.Extentions;
-using Aban360.CalculationPool.Application.Extensions;
-using Aban360.CalculationPool.Persistence.Extensions;
-using Aban360.MeterPool.Application.Extensions;
-using Aban360.MeterPool.Persistence.Extentions;
-using Aban360.WorkflowPool.Persistence.Extensions;
-using Aban360.WorkflowPool.Application.Extensions;
-using Aban360.BlobPool.Persistence.Extensions;
-using Aban360.BlobPool.Application.Extenstions;
-using Aban360.BlobPool.GatewayAddHoc.Extentions;
-using Aban360.SystemPool.Application.Extenstions;
 using Aban360.ReportPool.Application.Extensions;
 using Aban360.ReportPool.GatewayAdhoc.Extensions;
-using Aban360.InstallationPool.Application.Extensions;
-using Aban360.InstallationPool.Persistence.Extensions;
-using Aban360.PaymentPool.Application.Extensions;
-using Aban360.PaymentPool.Persistence.Extensions;
-using Aban360.CalculationPool.GatewayAdhoc.Extensions;
-using Aban360.ClaimPool.GatewayAdhoc.Extensions;
-
+using Aban360.LocationPool.Persistence.Extensions;
+using Aban360.LocationPool.Application.Extensions;
+using Aban360.LocationPool.GatewayAdhoc.Extentions;
+using Aban360.BlobPool.Persistence.Extensions;
+using Aban360.BlobPool.Application.Extenstions;
+using Aban260.BlobPool.Infrastructure.Extenstions;
+using Aban360.BlobPool.GatewayAddHoc.Extentions;
 namespace Aban360.BrdigeApi.Extensions
 {
     internal static class ConfigureDependencies
@@ -32,16 +17,9 @@ namespace Aban360.BrdigeApi.Extensions
         public static void AddDI(this IServiceCollection services)
         {
             services.AddUserPoolDI();
-            services.AddLocationPoolDI();
-            services.AddClaimPoolDI();
-            services.AddCalculationPoolDI();
-            services.AddMeterPoolDI();
-            services.AddWorkflowPoolDI();
-            services.AddBlobPoolDI();
-            services.AddSystemPoolDI();
             services.AddReportPoolDI();
-            services.AddInstallationPoolDI();
-            services.AddPaymentPoolDI();
+            services.AddLocationPoolDI();
+            services.AddBlobPoolDI();
         }
 
         private static void AddUserPoolDI(this IServiceCollection services)
@@ -49,63 +27,24 @@ namespace Aban360.BrdigeApi.Extensions
             services.AddUserPoolPersistenceInjections();
             services.AddUserPoolApplicationInjections();
         }
-        private static void AddLocationPoolDI(this IServiceCollection services)
-        {
-            services.AddLocationPoolPersistenceInjections();
-            services.AddLocationPoolApplicationInjections();
-            services.AddLocationPoolGatewayInjections();
-        }
-
-        private static void AddClaimPoolDI(this IServiceCollection services)
-        {
-            services.AddClaimPoolApplicationInjections();
-            services.AddClaimPoolPersistenceInjections();
-            services.AddClaimPoolGatewayInjections();
-        }
-
         private static void AddReportPoolDI(this IServiceCollection services)
         {
             services.AddReportPoolApplicationInjections();
             services.AddReportPoolPersistenceInjections();
             services.AddReportPoolGatewayInjections();
         }
-        private static void AddCalculationPoolDI(this IServiceCollection services)
+        private static void AddLocationPoolDI(this IServiceCollection services)
         {
-            services.AddCalculationPoolApplicationInjections();
-            services.AddCalculationPoolPersistenceInjections();
-            services.AddCalculationPoolGatewayInjections();
-        }
-        private static void AddMeterPoolDI(this IServiceCollection services)
-        {
-            services.AddMeterPoolApplicationInjections();
-            services.AddMeterPoolPersistenceInjections();
-        }
-        private static void AddWorkflowPoolDI(this IServiceCollection services)
-        {
-            services.AddWorkflowPoolApplicationInjections();
-            services.AddWorkflowPoolPersistenceInjections();
+            services.AddLocationPoolPersistenceInjections();
+            services.AddLocationPoolApplicationInjections();
+            services.AddLocationPoolGatewayInjections();
         }
         private static void AddBlobPoolDI(this IServiceCollection services)
         {
-            services.AddBlobPoolApplicationInjections();
             services.AddBlobPoolPersistenceInjections();
+            services.AddBlobPoolApplicationInjections();
+            services.AddBlobPoolInfrastructureInjections();
             services.AddBlobPoolGatewayInjections();
         }
-        private static void AddSystemPoolDI(this IServiceCollection services)
-        {
-            services.AddSystemPoolApplicationInjections();
-        }
-
-        private static void AddInstallationPoolDI(this IServiceCollection services)
-        {
-            services.AddInstallationPoolApplicationInjections();
-            services.AddInstallationPoolPersistenceInjections();
-        }
-        private static void AddPaymentPoolDI(this IServiceCollection services)
-        {
-            services.AddPaymentPoolApplicationInjections();
-            services.AddPaymentPoolPersistenceInjections();
-        }
-
     }
 }
