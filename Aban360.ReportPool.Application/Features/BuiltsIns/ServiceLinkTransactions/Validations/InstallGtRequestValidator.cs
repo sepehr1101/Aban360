566c573232eb6a7d9a9b2dff5b3c1862ae6223bb
ZEtehadi <zetehadi82@gmail.com>
2025-10-02 14:58:54 +0330

    InstallGtRequest Report

---
+++ Aban360.ReportPool.Application/Features/BuiltsIns/ServiceLinkTransactions/Validations/InstallGtRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using Aban360.Common.Literals;
+using Aban360.ReportPool.Application.Features.Base.Validations;
+using Aban360.ReportPool.Domain.Features.BuiltIns.ServiceLinkTransaction.Inputs;
+using FluentValidation;
+
+namespace Aban360.ReportPool.Application.Features.BuiltsIns.ServiceLinkTransactions.Validations
+{
+    public class InstallGtRequestValidator : BaseValidator<InstallGtRequestInputDto>
+    {
+        public InstallGtRequestValidator()
+        {
+            RuleFor(installGtRequest => installGtRequest.FromDateJalali)
+               .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+               .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(installGtRequest => installGtRequest.ToDateJalali)
+               .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+               .NotNull().WithMessage(ExceptionLiterals.NotNull);
+            
+            RuleFor(installGtRequest => installGtRequest.Distance)
+               .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+               .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(input => input)
+              .Must(input => FromToDateJalaliValidation.DateValidation(new FromToDateJalaliDto(input.FromDateJalali,
+                                                                                              input.ToDateJalali)).IsValid)
+              .WithMessage(input => FromToDateJalaliValidation.DateValidation(new FromToDateJalaliDto(input.FromDateJalali,
+                                                                                              input.ToDateJalali)).ErrorMessage);
+
+        }
+    }
+}
---
+++ Aban360.ReportPool.Domain/Features/BuiltIns/ServiceLinkTransaction/Outputs/InstallGtRequestDataOutputDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Aban360.ReportPool.Domain.Features.BuiltIns.ServiceLinkTransaction.Outputs
+{
+    public record InstallGtRequestDataOutputDto
+    {
+        public string WaterRequestDateJalali { get; set; }
+        public string WaterInstallDateJalali { get; set; }
+        public string CustomerNumber { get; set; }
+        public int ZoneId { get; set; }
+        public string ZoneTitle { get; set; }
+        public string BillId { get; set; }
+        public string FullName { get; set; }
+        public string ReadingNumber { get; set; }
+        public string UsageTitle { get; set; }
+        public int Distance { get; set; }
+    }
+}
