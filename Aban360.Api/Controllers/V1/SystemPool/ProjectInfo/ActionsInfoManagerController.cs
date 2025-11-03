1a20199d635236b37176b15e19981a2e49f873af
sepehr shamsaie <mantera.sh@hotmail.com>
2025-11-03 12:10:14 +0330

    get actions info

---
+++ Aban360.Api/Controllers/V1/SystemPool/ProjectInfo/ActionsInfoManagerController.cs
@@ -0,0 +1,18 @@
+﻿using Aban360.Api.Tools;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aban360.Api.Controllers.V1.SystemPool.ProjectInfo
+{
+    [Route("project")]
+    [AllowAnonymous]
+    public class ActionsInfoManagerController : BaseController
+    {
+        [HttpGet("actions-info")]
+        public IActionResult GetActionPath()
+        {
+            ControllerInspector.ExportControllerActions("ControllersAndActions.csv");
+            return Ok(@"Exported successfully to AppData/Excels");
+        }
+    }
+}
