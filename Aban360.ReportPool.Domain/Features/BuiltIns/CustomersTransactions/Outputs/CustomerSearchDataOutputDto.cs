cdddafdbb6dc037717b31b873d03d382d5ecf09c
ZEtehadi <zetehadi82@gmail.com>
2025-11-09 17:07:57 +0330

    CustomerSearchAdvance

--- Aban360.ReportPool.Domain/Features/BuiltIns/CustomersTransactions/Outputs/CustomerSearchDataOutputDto.cs
+++ Aban360.ReportPool.Domain/Features/BuiltIns/CustomersTransactions/Outputs/CustomerSearchDataOutputDto.cs
@@ -7,6 +7,7 @@
         public string? ReadingNumber { get; set; }
         public string? FirstName { get; set; }
         public string? Surname { get; set; }
+        public string? FatherName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? NationalCode { get; set; }
         public string? PostalCode { get; set; }
