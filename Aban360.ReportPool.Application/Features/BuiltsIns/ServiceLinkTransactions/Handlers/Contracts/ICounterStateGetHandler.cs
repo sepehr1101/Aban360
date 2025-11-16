5feda929d8e9ae60ab32b8109aa61ef50f5ffdcf
ZEtehadi <zetehadi82@gmail.com>
2025-11-16 13:54:02 +0330

    CounterStateGet

---
+++ Aban360.ReportPool.Application/Features/BuiltsIns/ServiceLinkTransactions/Handlers/Contracts/ICounterStateGetHandler.cs
@@ -0,0 +1,9 @@
+﻿using Aban360.Common.BaseEntities;
+
+namespace Aban360.ReportPool.Application.Features.BuiltsIns.ServiceLinkTransactions.Handlers.Contracts
+{
+    public interface ICounterStateGetHandler
+    {
+        Task<IEnumerable<StringDictionary>> Handle(CancellationToken cancellationToken);
+    }
+}
---
+++ Aban360.ReportPool.Persistence/Features/BuiltIns/PaymentTransactions/Implementations/CounterStateQueryService.cs
@@ -0,0 +1,32 @@
+﻿using Aban360.Common.BaseEntities;
+using Aban360.Common.Db.Dapper;
+using Aban360.ReportPool.Persistence.Features.BuiltIns.PaymentTransactions.Contracts;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Aban360.ReportPool.Persistence.Features.BuiltIns.PaymentTransactions.Implementations
+{
+    internal class CounterStateQueryService : AbstractBaseConnection, ICounterStateQueryService
+    {
+        public CounterStateQueryService(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        public async Task<IEnumerable<StringDictionary>> Get()
+        {
+            string query = GetQuery();
+            IEnumerable<StringDictionary> result = await _sqlReportConnection.QueryAsync<StringDictionary>(query);
+
+            return result;
+        }
+
+        private string GetQuery()
+        {
+            return @"Select 
+	                	MoshtarakinId as Id,
+	                	Title
+	                From [Db70].dbo.CounterVaziat ";
+        }
+    }
+}
