4f969c98b83feef052f239b82ec8515c8ec9ed09
ZEtehadi <zetehadi82@gmail.com>
2025-12-09 17:59:35 +0330

    ConsmptionSummary

--- Aban360.ReportPool.Application/Features/BuiltsIns/WaterTransactions/Handlers/Contracts/IConsumptionAverageManagementSummaryByUsageHandler.cs
+++ Aban360.ReportPool.Application/Features/BuiltsIns/WaterTransactions/Handlers/Contracts/IConsumptionAverageManagementSummaryByUsageHandler.cs
@@ -1,10 +1,13 @@
 ﻿using Aban360.Common.BaseEntities;
 using Aban360.ReportPool.Domain.Features.BuiltIns.WaterTransactions.Inputs;
+using Aban360.ReportPool.Domain.Features.BuiltIns.WaterTransactions.Outputs;
+using static Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Handlers.Implementations.ConsumptionAverageManagementSummaryByUsageHandler;
 
 namespace Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Handlers.Contracts
 {
     public interface IConsumptionAverageManagementSummaryByUsageHandler
     {
         Task<ReportOutput<ConsumptionAverageManagementHeaderOutputDto, ConsumptionAverageManagementSummaryDataOutputDto>> Handle(ConsumptionAverageManagementInputDto input, CancellationToken cancellationToken);
+        Task<ReportOutput<QuarterDto, KeyValueDto>> Handle(ConsumptionAverageManagementSummrayInputDto input, CancellationToken cancellationToken);
     }
 }
--- Aban360.ReportPool.Application/Features/BuiltsIns/WaterTransactions/Handlers/Implementations/ConsumptionAverageManagementSummaryByUsageHandler.cs
+++ Aban360.ReportPool.Application/Features/BuiltsIns/WaterTransactions/Handlers/Implementations/ConsumptionAverageManagementSummaryByUsageHandler.cs
@@ -4,6 +4,7 @@ using Aban360.Common.Extensions;
 using Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Handlers.Contracts;
 using Aban360.ReportPool.Domain.Base;
 using Aban360.ReportPool.Domain.Features.BuiltIns.WaterTransactions.Inputs;
+using Aban360.ReportPool.Domain.Features.BuiltIns.WaterTransactions.Outputs;
 using Aban360.ReportPool.Persistence.Features.BuiltIns.WaterTransactions.Contracts;
 using FluentValidation;
 using System.Linq;
@@ -13,21 +14,26 @@ namespace Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Ha
     internal sealed class ConsumptionAverageManagementSummaryByUsageHandler : IConsumptionAverageManagementSummaryByUsageHandler
     {
         private readonly IConsumptionAverageManagementSummaryByUsageQueryService _consumptionAverageManagerQueryService;
-        private readonly IValidator<ConsumptionAverageManagementInputDto> _validator;
+        private readonly IValidator<ConsumptionAverageManagementInputDto> _managementValidator;
+        private readonly IValidator<ConsumptionAverageManagementSummrayInputDto> _summaryValidator;
         public ConsumptionAverageManagementSummaryByUsageHandler(
             IConsumptionAverageManagementSummaryByUsageQueryService consumptionAverageManagerQueryService,
-            IValidator<ConsumptionAverageManagementInputDto> validator)
+            IValidator<ConsumptionAverageManagementInputDto> managementValidator,
+            IValidator<ConsumptionAverageManagementSummrayInputDto> summaryValidator)
         {
             _consumptionAverageManagerQueryService = consumptionAverageManagerQueryService;
             _consumptionAverageManagerQueryService.NotNull(nameof(consumptionAverageManagerQueryService));
 
-            _validator = validator;
-            _validator.NotNull(nameof(validator));
+            _managementValidator = managementValidator;
+            _managementValidator.NotNull(nameof(managementValidator));
+
+            _summaryValidator = summaryValidator;
+            _summaryValidator.NotNull(nameof(summaryValidator));
         }
 
         public async Task<ReportOutput<ConsumptionAverageManagementHeaderOutputDto, ConsumptionAverageManagementSummaryDataOutputDto>> Handle(ConsumptionAverageManagementInputDto input, CancellationToken cancellationToken)
         {
-            var validationResult = await _validator.ValidateAsync(input, cancellationToken);
+            var validationResult = await _managementValidator.ValidateAsync(input, cancellationToken);
             if (!validationResult.IsValid)
             {
                 var message = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
@@ -39,9 +45,15 @@ namespace Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Ha
             return result;
         }
 
-        public async Task Handle(ConsumptionAverageManagementSummrayInputDto input, CancellationToken cancellationToken)
+        public async Task<ReportOutput<QuarterDto, KeyValueDto>> Handle(ConsumptionAverageManagementSummrayInputDto input, CancellationToken cancellationToken)
         {
-            //validation
+            var validationResult = await _summaryValidator.ValidateAsync(input, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                var message = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
+                throw new CustomValidationException(message);
+            }
+
             IEnumerable<ConsumptionAverageManagementSummaryOutputDto> management = await _consumptionAverageManagerQueryService.Get(input);
 
             IEnumerable<KeyValueDto> usageGroupConsumptionAverage = management
@@ -53,31 +65,10 @@ namespace Aban360.ReportPool.Application.Features.BuiltsIns.WaterTransactions.Ha
             int between1_2ContOrOlgo = management.Count(m => m.ConsumptionAverage > m.ContracutalOrOlgo && m.ConsumptionAverage <= m.ContracutalOrOlgo * 2);
             int between2_3ContOrOlgo = management.Count(m => m.ConsumptionAverage > m.ContracutalOrOlgo * 2 && m.ConsumptionAverage <= m.ContracutalOrOlgo * 3);
             int moreThanContOrOlgo = management.Count(m => m.ConsumptionAverage > m.ContracutalOrOlgo * 3);
-            QuarterDto consumptionQuarter=new(lessThanContOrOlgo,between1_2ContOrOlgo,between2_3ContOrOlgo,moreThanContOrOlgo);
-        }
-        public record KeyValueDto
-        {
-            public string Title { get; }
-            public float Count { get; }
-            public KeyValueDto(string title, float count)
-            {
-                Title = title;
-                Count = count;
-            }
-        }
-        public record QuarterDto
-        {
-            public int LessThanContractualOrOlgo { get; set; }
-            public int Between1_2ContractualOrOlgo { get; set; }
-            public int Between2_3ContractualOrOlgo { get; set; }
-            public int MoreThanContractualOrOlgo { get; set; }
-            public QuarterDto(int lessThanContractualOrOlgo, int between1_2ContractualOrOlgo, int between2_3ContractualOrOlgo, int moreThanContractualOrOlgo)
-            {
-                LessThanContractualOrOlgo = lessThanContractualOrOlgo;
-                Between1_2ContractualOrOlgo = between1_2ContractualOrOlgo;
-                Between2_3ContractualOrOlgo = between2_3ContractualOrOlgo;
-                MoreThanContractualOrOlgo = moreThanContractualOrOlgo;
-            }
+            QuarterDto consumptionQuarter = new(lessThanContOrOlgo, between1_2ContOrOlgo, between2_3ContOrOlgo, moreThanContOrOlgo);
+
+            ReportOutput<QuarterDto, KeyValueDto> result = new(ReportLiterals.ConsumptionManagerSummary, consumptionQuarter, usageGroupConsumptionAverage);
+            return result;
         }
     }
 }
