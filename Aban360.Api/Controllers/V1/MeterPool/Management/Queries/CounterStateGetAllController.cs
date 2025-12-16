a1f2967512f94e6be5603f71f15e72ae343daed0
ZEtehadi <zetehadi82@gmail.com>
2025-12-16 10:34:40 +0330

    CouterStateGetAll Db70

--- Aban360.Api/Controllers/V1/MeterPool/Management/Queries/CounterStateGetAllController.cs
+++ Aban360.Api/Controllers/V1/MeterPool/Management/Queries/CounterStateGetAllController.cs
@@ -1,27 +1,44 @@
 ﻿using Aban360.Common.Categories.ApiResponse;
 using Aban360.Common.Extensions;
-using Aban360.MeterPool.Application.Features.Management.Handlers.Queries.Contracts;
+using meterPool = Aban360.MeterPool.Application.Features.Management.Handlers.Queries.Contracts;
+using oldCalcPool = Aban360.OldCalcPool.Application.Features.Db70.Handlers.Queries.Contracts;
 using Aban360.MeterPool.Domain.Features.Management.Dtos.Queries;
 using Microsoft.AspNetCore.Mvc;
+using Aban360.OldCalcPool.Domain.Features.Db70.Dto.Queries;
 
 namespace Aban360.Api.Controllers.V1.MeterPool.Management.Queries
 {
     [Route("v1/counter-state")]
     public class CounterStateGetAllController : BaseController
     {
-        private readonly ICounterStateGetAllHandler _counterStateGetAllHandler;
-        public CounterStateGetAllController(ICounterStateGetAllHandler counterStateGetAllHandler)
+        private readonly meterPool.ICounterStateGetAllHandler _counterStateGetAllMeterPoolHandler;
+        private readonly oldCalcPool.ICounterStateGetAllHandler _counterStateGetAllOldCalcPoolHandler;
+        public CounterStateGetAllController(
+            meterPool.ICounterStateGetAllHandler counterStateGetAllMeterPoolHandler,
+            oldCalcPool.ICounterStateGetAllHandler counterStateGetAllOldCalcPoolHandler)
         {
-            _counterStateGetAllHandler = counterStateGetAllHandler;
-            _counterStateGetAllHandler.NotNull(nameof(counterStateGetAllHandler));
+            _counterStateGetAllMeterPoolHandler = counterStateGetAllMeterPoolHandler;
+            _counterStateGetAllMeterPoolHandler.NotNull(nameof(counterStateGetAllMeterPoolHandler));
+
+            _counterStateGetAllOldCalcPoolHandler = counterStateGetAllOldCalcPoolHandler;
+            _counterStateGetAllOldCalcPoolHandler.NotNull(nameof(counterStateGetAllOldCalcPoolHandler));
+        }
+
+        [HttpPost, HttpGet]
+        [Route("all-aban360")]
+        [ProducesResponseType(typeof(ApiResponseEnvelope<ICollection<CounterStateGetDto>>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllAban360(CancellationToken cancellationToken)
+        {
+            ICollection<CounterStateGetDto> counterStates = await _counterStateGetAllMeterPoolHandler.Handle(cancellationToken);
+            return Ok(counterStates);
         }
 
         [HttpPost, HttpGet]
         [Route("all")]
-        [ProducesResponseType(typeof(ApiResponseEnvelope<ICollection<CounterStateGetDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponseEnvelope<IEnumerable<CounterStateCodeDto>>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
         {
-            ICollection<CounterStateGetDto> counterStates = await _counterStateGetAllHandler.Handle(cancellationToken);
+            IEnumerable<CounterStateCodeDto> counterStates = await _counterStateGetAllOldCalcPoolHandler.Handle(cancellationToken);
             return Ok(counterStates);
         }
     }
---
+++ Aban360.OldCalcPool.Persistence/Features/Db70/Queries/Implementations/CounterStateQueryService.cs
@@ -0,0 +1,53 @@
+﻿using Aban360.Common.Db.Dapper;
+using Aban360.Common.Db.Exceptions;
+using Aban360.OldCalcPool.Domain.Features.Db70.Dto.Queries;
+using Aban360.OldCalcPool.Persistence.Features.Db70.Queries.Contracts;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Aban360.OldCalcPool.Persistence.Features.Db70.Queries.Implementations
+{
+    internal sealed class CounterStateQueryService : AbstractBaseConnection, ICounterStateQueryService
+    {
+        public CounterStateQueryService(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        public async Task<CounterStateCodeDto> Get(int id)
+        {
+            string query = GetSingleQuery();
+            CounterStateCodeDto result = await _sqlReportConnection.QueryFirstOrDefaultAsync<CounterStateCodeDto>(query, new { id });
+            if (result is null)
+            {
+                throw new InvalidIdException();
+            }
+
+            return result;
+        }
+
+        public async Task<IEnumerable<CounterStateCodeDto>> Get()
+        {
+            string query = GetAllQuery();
+            IEnumerable<CounterStateCodeDto> result = await _sqlReportConnection.QueryAsync<CounterStateCodeDto>(query, null);
+
+            return result;
+        }
+
+        private string GetSingleQuery()
+        {
+            return @"Select 
+                    	MoshtarakinId as Id,
+                    	Title	
+                    From [Db70].dbo.CounterVaziat
+                    Where MoshtarakinId=@id";
+        }
+        private string GetAllQuery()
+        {
+            return @"Select 
+                    	MoshtarakinId as Id,
+                    	Title	
+                    From [Db70].dbo.CounterVaziat";
+        }
+    }
+}
