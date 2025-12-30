ea5ae35f49833ecec0af348906ec7c472c05fdd3
ZEtehadi <zetehadi82@gmail.com>
2025-12-30 15:42:28 +0330

    Bridge: ServiceLinkInquiry/RegisterPaymentManual

---
+++ Aban360.CalculationPool.Application/Features/ServiceLink/Handler/Commands/Implementations/ServiceLinkRegisterManualHandler.cs
@@ -0,0 +1,39 @@
+﻿using Aban360.CalculationPool.Application.Features.ServiceLink.Handler.Commands.Contracts;
+using Aban360.CalculationPool.Domain.Features.MeterReading.Dtos.Commands;
+using Aban360.CalculationPool.Domain.Features.ServiceLink;
+using Aban360.CalculationPool.Persistence.Features.MeterReading.Contracts;
+using Aban360.Common.Exceptions;
+using Aban360.Common.Extensions;
+using FluentValidation;
+
+namespace Aban360.CalculationPool.Application.Features.ServiceLink.Handler.Commands.Implementations
+{
+    internal sealed class ServiceLinkRegisterManualHandler : IServiceLinkRegisterManualHandler
+    {
+        private readonly IValidator<ServiceLinkRegisterManualInputDto> _validator;
+        private readonly ICustomerInfoService _customerInfoService;
+        public ServiceLinkRegisterManualHandler(
+            IValidator<ServiceLinkRegisterManualInputDto> validator,
+            ICustomerInfoService customerInfoService)
+        {
+            _validator = validator;
+            _validator.NotNull(nameof(validator));
+
+            _customerInfoService = customerInfoService;
+            _customerInfoService.NotNull(nameof(customerInfoService));
+        }
+
+        public async Task Handle(ServiceLinkRegisterManualInputDto input, CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(input, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                var message = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
+                throw new CustomValidationException(message);
+            }
+
+            ZoneIdAndCustomerNumberGetDto zoneIdAndCustomerNumber = await _customerInfoService.GetZoneIdAndCustomerNumber(input.BillId);
+            //register
+        }
+    }
+}
---
+++ Aban360.CalculationPool.Domain/Features/ServiceLink/ServiceLinkInquiryInputDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Aban360.CalculationPool.Domain.Features.ServiceLink
+{
+    public record ServiceLinkInquiryInputDto
+    {
+        public string PaymentId { get; set; }
+        public string BillId { get; set; }
+    }
+}
