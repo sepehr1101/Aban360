0ccc29b3fbf85e127c9c115ccf81752a41daba71
ZEtehadi <zetehadi82@gmail.com>
2026-02-18 16:37:10 +0330

    BillInstallment BugFixed

--- Aban360.OldCalcPool.Domain/Features/Processing/Dto/Commands/BillInstallmentDataOutputDto.cs
+++ Aban360.OldCalcPool.Domain/Features/Processing/Dto/Commands/BillInstallmentDataOutputDto.cs
@@ -2,9 +2,12 @@
 {
     public record BillInstallmentDataOutputDto
     {
-        public string DeadLineDateJalali { get; set; }
+        public string DeadLineDateJalali { get; set; } = default!;
         public long Payable { get; set; }
         public int QueueNumber { get; set; }
+        public string QueueNumberTitle { get; set; }
+        public string BillId { get; set; } = default!;
+        public string PaymentId { get; set; } = default!;
 
     }
 }
--- Aban360.OldCalcPool.Domain/Features/WaterReturn/Dto/Queries/MemberGetDto.cs
+++ Aban360.OldCalcPool.Domain/Features/WaterReturn/Dto/Queries/MemberGetDto.cs
@@ -17,6 +17,11 @@
         public string FullName { get; set; }
         public string UsageTitle { get; set; }
         public string ZoneTitle { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? NationalCode { get; set; }
+        public long LatestDebt { get; set; }
+        public string BillId { get; set; }
 
     }
 }
