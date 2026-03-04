b4bfee2153a5cc062f9046412cf1b4591315eb93
ZEtehadi <zetehadi82@gmail.com>
2026-03-04 17:12:44 +0330

    MeterReading BugFixed

---
+++ Aban360.CalculationPool.Domain/Features/MeterReading/Dtos/Commands/MeterFlowDeleteDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Aban360.CalculationPool.Domain.Features.MeterReading.Dtos.Commands
+{
+    public record MeterFlowDeleteDto
+    {
+        public int Id { get; set; }
+        public Guid RemovedByUserId { get; set; }
+        public DateTime RemovedDateTime { get; set; }
+        public MeterFlowDeleteDto(int id, Guid removedByUserId, DateTime removedDateTime)
+        {
+            Id=id;
+            RemovedByUserId=removedByUserId;
+            RemovedDateTime=removedDateTime;
+        }
+    }
+}
