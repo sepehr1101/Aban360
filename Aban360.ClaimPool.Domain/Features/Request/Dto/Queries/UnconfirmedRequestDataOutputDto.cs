63cf7489fde2dc5d0faf0bbf45a5c14c359b3d15
ZEtehadi <zetehadi82@gmail.com>
2026-04-13 12:54:07 +0330

    UnconfirmedRequestByZone

---
+++ Aban360.ClaimPool.Domain/Features/Request/Dto/Queries/UnconfirmedRequestDataOutputDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Aban360.ClaimPool.Domain.Features.Request.Dto.Queries
+{
+    public record UnconfirmedRequestDataOutputDto
+    {
+        public int TrackNumber { get; set; }
+        public long Amount { get; set; }
+        public int RegionId { get; set; }
+        public string RegionTitle { get; set; }
+        public int ZoneId { get; set; }
+        public string ZoneTitle { get; set; }
+        public string BillId { get; set; }
+        public int CustomerNumber { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string FullName { get; set; }
+        public string NationalCode { get; set; }
+        public int UsageId { get; set; }
+        public string UsageTitle { get; set; }
+    }
+}
---
+++ Aban360.ClaimPool.Domain/Features/Request/Dto/Queries/UnconfirmedRequestHeaderOutputDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Aban360.ClaimPool.Domain.Features.Request.Dto.Queries
+{
+    public record UnconfirmedRequestHeaderOutputDto
+    {
+        public int ZoneId { get; set; }
+        public string ZoneTitle { get; set; }
+        public int RecordCount { get; set; }
+        public string Title { get; set; }
+        public string ReportDateJalali { get; set; }
+        public long Amount { get; set; }
+    }
+}
