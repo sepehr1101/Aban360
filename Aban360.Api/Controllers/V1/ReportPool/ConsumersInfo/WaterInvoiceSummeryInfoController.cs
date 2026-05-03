af8f0bfeae6842459f954ea0be20f28409a0c2fe
sepehr shamsaie <mantera.sh@hotmail.com>
2026-05-03 12:41:53 +0400

    bug fix

--- Aban360.Api/Controllers/V1/ReportPool/ConsumersInfo/WaterInvoiceSummeryInfoController.cs
+++ Aban360.Api/Controllers/V1/ReportPool/ConsumersInfo/WaterInvoiceSummeryInfoController.cs
@@ -38,7 +38,7 @@ namespace Aban360.Api.Controllers.V1.ReportPool.ConsumersInfo
         }
 
         [HttpPost]
-        [Route("summery-2-by-id")]
+        [Route("summary-2-by-id")]
         [ProducesResponseType(typeof(ApiResponseEnvelope<ReportOutput<WaterInvoiceDto, LineItemsDto>>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetSummary2ById([FromBody] DisplayThisBillInput searchInput)
         {
@@ -47,7 +47,7 @@ namespace Aban360.Api.Controllers.V1.ReportPool.ConsumersInfo
         }
 
         [HttpPost]
-        [Route("summery-2-withLastDb")]
+        [Route("summary-2-withLastDb")]
         [ProducesResponseType(typeof(ApiResponseEnvelope<ReportOutput<WaterInvoiceDto, LineItemsDto>>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetSummary2WithLastDb([FromBody] SearchInput searchInput, CancellationToken cancellationToken)
         {
