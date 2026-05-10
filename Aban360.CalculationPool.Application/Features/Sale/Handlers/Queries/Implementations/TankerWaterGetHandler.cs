0e0288d974111566bc60e2808ea15a954f53b52c
ZEtehadi <zetehadi82@gmail.com>
2026-05-10 12:11:49 +0330

    TankerGet

---
+++ Aban360.CalculationPool.Application/Features/Sale/Handlers/Queries/Implementations/TankerWaterGetHandler.cs
@@ -0,0 +1,25 @@
+﻿using Aban360.CalculationPool.Application.Features.Sale.Handlers.Queries.Contracts;
+using Aban360.CalculationPool.Domain.Features.Sale.Dto.Input;
+using Aban360.CalculationPool.Domain.Features.Sale.Dto.Output;
+using Aban360.CalculationPool.Persistence.Features.Sale.Queries.Contracts;
+using Aban360.Common.BaseEntities;
+using Aban360.Common.Extensions;
+
+namespace Aban360.CalculationPool.Application.Features.Sale.Handlers.Queries.Implementations
+{
+    internal sealed class TankerWaterGetHandler : ITankerWaterGetHandler
+    {
+        private readonly ITankerQueryService _tankerQueryService;
+        public TankerWaterGetHandler(ITankerQueryService tankerQueryService)
+        {
+            _tankerQueryService = tankerQueryService;
+            _tankerQueryService.NotNull(nameof(tankerQueryService));
+        }
+
+        public async Task<ReportOutput<TankerWaterHeaderOutputDto, TankerWaterDateOutputDto>> Handle(TankerWaterInputDto inputDto, CancellationToken cancellationToken)
+        {
+            ReportOutput<TankerWaterHeaderOutputDto, TankerWaterDateOutputDto> result = await _tankerQueryService.Get(inputDto);
+            return result;
+        }
+    }
+}
--- Aban360.CalculationPool.Domain/Features/Sale/Dto/Output/TankerWaterDateOutputDto.cs
+++ Aban360.CalculationPool.Domain/Features/Sale/Dto/Output/TankerWaterDateOutputDto.cs
@@ -2,6 +2,19 @@
 {
     public record TankerWaterDateOutputDto
     {
-
+        public int RegionId { get; set; }
+        public string RegionTitle { get; set; }
+        public int ZoneId { get; set; }
+        public string ZoneTitle { get; set; }
+        public int CustomerNumber { get; set; }
+        public string FirstName { get; set; }
+        public string? Surname { get; set; }
+        public string FullName { get; set; }
+        public string? Address { get; set; }
+        public int Barge { get; set; }
+        public int Consumption { get; set; }
+        public long Amount { get; set; }
+        public string RegisterDateJalali { get; set; }
+        public bool IsNotShorb { get; set; }
     }
 }
