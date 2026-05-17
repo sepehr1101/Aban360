699182eb8cd92bc1aefbc5acbece2c2ff67ea373
sepehr shamsaie <mantera.sh@hotmail.com>
2026-05-17 15:21:11 +0400

    display tiff image

--- Aban360.BlobPool.Application/Features/OpenKm/Handlers/Queries/Implementations/GetDocumentBinaryHandler.cs
+++ Aban360.BlobPool.Application/Features/OpenKm/Handlers/Queries/Implementations/GetDocumentBinaryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Aban260.BlobPool.Infrastructure.Providers.OpenKm.Contracts;
 using Aban360.BlobPool.Application.Features.OpenKm.Handlers.Queries.Contracts;
 using Aban360.Common.Extensions;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Aban360.BlobPool.Application.Features.OpenKm.Handlers.Queries.Implementations
 {
@@ -15,8 +17,31 @@ namespace Aban360.BlobPool.Application.Features.OpenKm.Handlers.Queries.Implemen
         }
         public async Task<byte[]> Handle(string documentId, bool isThumbnail, CancellationToken cancellationToken)
         {
-            return isThumbnail ? await _openKmQueryService.GetImageThumbnail(documentId) :
+            byte[] image= isThumbnail ? await _openKmQueryService.GetImageThumbnail(documentId) :
                 await _openKmQueryService.GetFileBinary(documentId);
+            if (IsTiff(image))
+            {
+                using var inputStream = new MemoryStream(image);
+                using var bitmap = new Bitmap(inputStream);
+
+                using var outputStream = new MemoryStream();
+                bitmap.Save(outputStream, ImageFormat.Jpeg);
+
+                return outputStream.ToArray();
+            }
+            return image;
+        }
+        private bool IsTiff(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length < 4)
+                return false;
+
+            return
+                // Little-endian: "II" 2A 00
+                (bytes[0] == 0x49 && bytes[1] == 0x49 && bytes[2] == 0x2A && bytes[3] == 0x00) ||
+
+                // Big-endian:    "MM" 00 2A
+                (bytes[0] == 0x4D && bytes[1] == 0x4D && bytes[2] == 0x00 && bytes[3] == 0x2A);
         }
     }
 }
