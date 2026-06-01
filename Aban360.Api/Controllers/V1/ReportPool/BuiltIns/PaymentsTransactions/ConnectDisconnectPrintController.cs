1c01f31f5406269760002b6dfb77a33f90c2acbb
ZEtehadi <zetehadi82@gmail.com>
2026-06-01 15:42:13 +0330

    Kardex, DeleteUnconfirmedReturn, ConnectDisconnectInComplete

--- Aban360.Api/Controllers/V1/ReportPool/BuiltIns/PaymentsTransactions/ConnectDisconnectPrintController.cs
+++ Aban360.Api/Controllers/V1/ReportPool/BuiltIns/PaymentsTransactions/ConnectDisconnectPrintController.cs
@@ -13,13 +13,18 @@ namespace Aban360.Api.Controllers.V1.ReportPool.BuiltIns.PaymentsTransactions
     public class ConnectDisconnectPrintController : BaseController
     {
         private readonly IConnectDisconnectPrintHandler _connectDisconnectPrint;
+        private readonly IPendingPaymentsPrintsHandler _pendingPaymentsPrintsHandler;
         private readonly IReportGenerator _reportGenerator;
         public ConnectDisconnectPrintController(
             IConnectDisconnectPrintHandler connectDisconnectPrint,
+            IPendingPaymentsPrintsHandler pendingPaymentsPrintsHandler,
             IReportGenerator reportGenerator)
         {
             _connectDisconnectPrint = connectDisconnectPrint;
             _connectDisconnectPrint.NotNull(nameof(_connectDisconnectPrint));
+
+            _pendingPaymentsPrintsHandler=pendingPaymentsPrintsHandler;
+            _pendingPaymentsPrintsHandler.NotNull(nameof(pendingPaymentsPrintsHandler));
 
             _reportGenerator = reportGenerator;
             _reportGenerator.NotNull(nameof(_reportGenerator));
@@ -28,7 +33,7 @@ namespace Aban360.Api.Controllers.V1.ReportPool.BuiltIns.PaymentsTransactions
         [HttpPost]
         [Route("connect-sti")]
         [ProducesResponseType(typeof(ApiResponseEnvelope<JsonReportId>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetConnectStiReport(ConnectDisconnectPrintInputDto inputDto, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetSingleConnectStiReport(ConnectDisconnectPrintInputDto inputDto, CancellationToken cancellationToken)
         {
             int reportCode = 2070;
             ConnectDisconnectPrintOutputDto result = await _connectDisconnectPrint.Handle(inputDto, cancellationToken);
@@ -39,11 +44,33 @@ namespace Aban360.Api.Controllers.V1.ReportPool.BuiltIns.PaymentsTransactions
         [HttpPost]
         [Route("disconnect-sti")]
         [ProducesResponseType(typeof(ApiResponseEnvelope<JsonReportId>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetDisconnectStiReport(ConnectDisconnectPrintInputDto inputDto, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetSingleDisconnectStiReport(ConnectDisconnectPrintInputDto inputDto, CancellationToken cancellationToken)
         {
             int reportCode = 2071;
             ConnectDisconnectPrintOutputDto result = await _connectDisconnectPrint.Handle(inputDto, cancellationToken);
             JsonReportId reportId = await JsonOperation.ExportToJsonFlat(result, cancellationToken, reportCode);
+            return Ok(reportId);
+        }
+
+        [HttpPost]
+        [Route("connect-list-sti")]
+        [ProducesResponseType(typeof(ApiResponseEnvelope<JsonReportId>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetConnectStiReport(PendingPaymentsInputDto inputDto, CancellationToken cancellationToken)
+        {
+            int reportCode = 2070;
+            ReportOutput<PendingPaymentsPrintstHeaderOutputDto, PendingPaymentPrintsDataOutputDto> result = await _pendingPaymentsPrintsHandler.Handle(inputDto, cancellationToken);
+            JsonReportId reportId = await JsonOperation.ExportToJson(result, cancellationToken, reportCode);
+            return Ok(reportId);
+        }
+
+        [HttpPost]
+        [Route("disconnect-list-sti")]
+        [ProducesResponseType(typeof(ApiResponseEnvelope<JsonReportId>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetDisconnectStiReport(PendingPaymentsInputDto inputDto, CancellationToken cancellationToken)
+        {
+            int reportCode = 2071;
+            ReportOutput<PendingPaymentsPrintstHeaderOutputDto, PendingPaymentPrintsDataOutputDto> result = await _pendingPaymentsPrintsHandler.Handle(inputDto, cancellationToken);
+            JsonReportId reportId = await JsonOperation.ExportToJson(result, cancellationToken, reportCode);
             return Ok(reportId);
         }
     }
