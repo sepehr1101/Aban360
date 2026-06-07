53d3fd5722e9aecac48d3f6cedfa9df4c44e4548
ZEtehadi <zetehadi82@gmail.com>
2026-06-07 16:02:35 +0330

    ConnctDisconnectPrint

--- Aban360.ReportPool.Domain/Features/BuiltIns/PaymentsTransactions/Outputs/PendingPaymentPrintsDataOutputDto.cs
+++ Aban360.ReportPool.Domain/Features/BuiltIns/PaymentsTransactions/Outputs/PendingPaymentPrintsDataOutputDto.cs
@@ -2,6 +2,8 @@
 {
     public record PendingPaymentPrintsDataOutputDto
     {
+        public string RegionTitle { get; set; }
+        public string ZoneTitle { get; set; }
         public int CustomerNumber { get; set; }
         public int CustomerNumber2 { get; set; }
         public string ReadingNumber { get; set; }
@@ -9,8 +11,12 @@
         public string EmptyDueDateJalali { get; set; } = string.Empty;
         public string FirstName { get; set; }
         public string Surname { get; set; }
+        public string FullName { get; set; }
+        public string? FatherName { get; set; }
+        public string? NationalCode{ get; set; }
         public string? Address { get; set; }
         public int UsageId { get; set; }
+        public string UsageTitle { get; set; }
         public string? PhoneNumber { get; set; }
         public string? MobileNumber { get; set; }
         public int DebtPeriodCount { get; set; }
@@ -19,6 +25,6 @@
         public string DueDateJalali { get; set; }
         public string BillId { get; set; }
         public string PayId { get; set; }
-
+        public string CauseTitle { get; set; }
     }
 }
