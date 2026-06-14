bc7a20ed1794d5c59cc2d3a1bafe369a6dec0849
ZEtehadi <zetehadi82@gmail.com>
2026-06-14 16:55:38 +0330

    Merge

--- Aban360.ReportPool.Persistence/Features/BuiltIns/PaymentTransactions/Implementations/PendingPaymentsQueryService.cs
+++ Aban360.ReportPool.Persistence/Features/BuiltIns/PaymentTransactions/Implementations/PendingPaymentsQueryService.cs
@@ -71,13 +71,8 @@ namespace Aban360.ReportPool.Persistence.Features.BuiltIns.PaymentTransactions.I
         private string GetPendingPaymentsDataQuery(bool hasUsageSellId,bool hasUsageConsumptionId)
         {
 			string usageSellQuery = hasUsageSellId == true ? "AND (UsageId IN @UsageSellIds)" : string.Empty;
-<<<<<<< HEAD
-			string usageConsumptionQuery = hasUsageConsumptionId == true ? "AND (UsageId2 IN @UsageConsumptionIds)" : string.Empty;
-           return @$"-- مشتریان هدف
-=======
 			string usageConsumptionQuery = hasUsageConsumptionId == true ? "AND ((UsageId2 IN @UsageConsumptionIds) OR (UsageId IN @UsageConsumptionIds AND UsageId2=0))" : string.Empty;
             return @$"-- مشتریان هدف
->>>>>>> fe701a6b592400c750c426013baa72c0c58e96b7
 						WITH FilteredClients AS (
 							SELECT 
 								ZoneId,
