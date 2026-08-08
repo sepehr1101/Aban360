24869d780773548d8b340cb2f37c951b25db7f17
ZEtehadi <zetehadi82@gmail.com>
2026-08-09 00:42:17 +0330

    MeterReading BugFixed

--- Aban360.OldCalcPool.Domain/Features/WaterReturn/Dto/Queries/BedBesPreviousNumberAndDateOutputDto.cs
+++ Aban360.OldCalcPool.Domain/Features/WaterReturn/Dto/Queries/BedBesPreviousNumberAndDateOutputDto.cs
@@ -4,10 +4,18 @@
     {
         public string PreviousDateJalali { get; set; }
         public int PreviousNumber { get; set; }
-        public BedBesPreviousNumberAndDateOutputDto(string previousDateJalali, int previousNumber)
+        public int CounterStateCode { get; set; }
+        public string CounterStateTitle { get; set; }
+        public int Consumption { get; set; }
+        public float ConsumptionAverage { get; set; }
+        public BedBesPreviousNumberAndDateOutputDto(string previousDateJalali, int previousNumber, int counterStateCode, string counterStateTitle, int consumption, float consumptionAverage)
         {
             PreviousDateJalali = previousDateJalali;
             PreviousNumber = previousNumber;
+            CounterStateCode = counterStateCode;
+            CounterStateTitle = counterStateTitle;
+            Consumption = consumption;
+            ConsumptionAverage = consumptionAverage;
         }
         public BedBesPreviousNumberAndDateOutputDto()
         {
