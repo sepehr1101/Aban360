3cd0f09c9b65cb428343b4a63eaad4d96121823c
ZEtehadi <zetehadi82@gmail.com>
2026-08-18 10:53:04 +0330

    CustomeUpdateValidation

---
+++ Aban360.ClaimPool.Application/Features/Land/Validations/CustomerBranchTypeUpdateValidator.cs
@@ -0,0 +1,17 @@
+﻿using Aban360.ClaimPool.Application.Features.Base.Validations;
+using Aban360.ClaimPool.Domain.Features.Land.Dto.Commands;
+using Aban360.Common.Literals;
+using FluentValidation;
+
+namespace Aban360.ClaimPool.Application.Features.Land.Validations
+{
+    public class CustomerBranchTypeUpdateValidator : BaseValidator<CustomerBranchTypeUpdateInputDto>
+    {
+        public CustomerBranchTypeUpdateValidator()
+        {
+            RuleFor(f => f.BillId)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+        }
+    }
+}
---
+++ Aban360.ClaimPool.Application/Features/Land/Validations/CustomerEsatateUpdateValidator.cs
@@ -0,0 +1,30 @@
+﻿using Aban360.ClaimPool.Application.Features.Base.Validations;
+using Aban360.ClaimPool.Domain.Features.Land.Dto.Commands;
+using Aban360.Common.Literals;
+using FluentValidation;
+
+namespace Aban360.ClaimPool.Application.Features.Land.Validations
+{
+    public class CustomerEsatateUpdateValidator : BaseValidator<CustomerEstateUpdateDto>
+    {
+        public CustomerEsatateUpdateValidator()
+        {
+            RuleFor(f => f.Id)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(f => f.ZoneId)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(f => f.CustomerNumber)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+            RuleFor(f => f.BillId)
+                .NotEmpty().WithMessage(ExceptionLiterals.NotNull)
+                .NotNull().WithMessage(ExceptionLiterals.NotNull);
+
+        }
+    }
+}
