383a38a8e66ea779eea50b254ad0f48f07538800
sepehr shamsaie <mantera.sh@hotmail.com>
2026-08-24 23:40:52 +0400

    household

--- Aban360.CalculationPool.Application/Features/MeterReading/Handlers/Commands/Update/Implementations/MeterReadingDetailUpdateHandler.cs
+++ Aban360.CalculationPool.Application/Features/MeterReading/Handlers/Commands/Update/Implementations/MeterReadingDetailUpdateHandler.cs
@@ -601,7 +601,11 @@ namespace Aban360.CalculationPool.Application.Features.MeterReading.Handlers.Com
             {
                 throw new InvalidDateException(readingDateJalali);
             }
-            if (expireHouseHoldGregorian.Value.AddYears(1) < readingDateGregorian.Value)
+            if (readingDateGregorian.Value < expireHouseHoldGregorian.Value)//تاریخ قرائت قبل از تاریخ ثبت خانوار
+            {
+                return 0;
+            }
+            if (expireHouseHoldGregorian.Value.AddYears(1) < readingDateGregorian.Value)// تاریخ قرائت بعد از تاریخ انقضای خانوار
             {
                 return 0;
             }
